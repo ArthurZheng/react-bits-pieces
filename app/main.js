5f310bde7aa10af9727e1fb1587efd5a4a825dfa
Jun Zheng <arthurjordan@gmail.com>
2016-04-18 21:44:11 +1000

    incrementCounter, FancyButton

--- app/main.js
+++ app/main.js
@@ -1,19 +1,37 @@
 import '../public/css/style.css';
-
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-let CommentBox = React.createClass({
+let IncrementCounter = React.createClass({
+
+    getInitialState: function(){
+      return { counter: 0}
+    },
+
+    increment: function(event){
+      this.setState({ counter: this.state.counter += 2 });
+    },
+
+    render: function(){
+        return (
+          <div class='increment-counter'>
+            Counter is {this.state.counter}
+            <FancyButton onClick={this.increment} icon='fa-arrow-circle-o-up' text='Fancy Button with FontAwesome Icon'/>
+          </div>
+        )
+    }
+});
+
+let FancyButton = React.createClass({
   render: function(){
-    return (
-      <div className='commentBox'>
-        <h1>Juns CommentBox 1213</h1>
-      </div>
-    )
-  },
+    return <button onClick={this.props.onClick}>
+            <i className={'fa' + this.props.icon}></i>
+            <span>{this.props.text}</span>
+        </button>
+  }
 })
 
 ReactDOM.render(
-  <CommentBox />,
+  <IncrementCounter />,
   document.getElementById('content')
 );
